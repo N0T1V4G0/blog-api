0e1eeaecefca204fde6c35ddbb9301b6b1a3bf9c
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-25 20:26:08 -0300

    feat: add in memory user repository

---
+++ src/modules/Users/repositories/InMemoryUsersRepository.js
@@ -0,0 +1,26 @@
+class InMemoryUsersRepository {
+  constructor() {
+    this.repository = [];
+  }
+
+  async create(user) {
+    const { displayName, email, password, image } = user;
+    const id = Math.floor(Math.random() * 9999) + 1;
+    const newUser = { id, displayName, email, password, image };
+    this.repository.push(newUser);
+    return newUser;
+  }
+
+  async findByEmail(email) {
+    return this.repository.find((e) => e.email === email);
+  }
+
+  async list() {
+    return this.repository;
+  }
+}
+
+const inMemoryUsersRepo = new InMemoryUsersRepository();
+Object.freeze(inMemoryUsersRepo);
+
+module.exports = inMemoryUsersRepo;
--- src/modules/Users/useCases/CreateUser/index.js
+++ src/modules/Users/useCases/CreateUser/index.js
@@ -1,9 +1,9 @@
-const UsersRepository = require('../../repositories/UsersRepository');
+const inMemoryUsersRepository = require('../../repositories/InMemoryUsersRepository');
 const CreateUserController = require('./CreateUserController');
 const CreateUserService = require('./CreateUserService');
 
-const userRepository = new UsersRepository();
-const createUserService = new CreateUserService(userRepository);
+// const userRepository = new UsersRepository();
+const createUserService = new CreateUserService(inMemoryUsersRepository);
 const createUserController = new CreateUserController(createUserService);
 
 module.exports = createUserController;

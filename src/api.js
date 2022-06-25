8cca4c46bff83371745f3782669a9edaae5f3e21
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-25 20:34:26 -0300

    feat: add AppError & error middleware

--- src/api.js
+++ src/api.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const morgan = require('morgan');
+const errorMiddleware = require('./middlewares/error');
+const createCategoryController = require('./modules/Category/useCases');
+const createUserController = require('./modules/Users/useCases/CreateUser');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 // ...
+app.post('/user', (req, res, next) => {
+  createUserController.handle(req, res, next);
+});
 
+app.post('/categories', (req, res, next) => {
+  createCategoryController.handle(req, res, next);
+});
+
+app.post('/categories', (req, res, next) => {
+  createCategoryController.handle(req, res, next);
+});
+
+app.use(errorMiddleware);
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;

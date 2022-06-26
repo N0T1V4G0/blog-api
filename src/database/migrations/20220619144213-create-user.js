01f634000d44e1795ad553551ddbce1adbf69fb7
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-26 03:33:15 -0300

    refactor: remove timestamps from user & category

--- src/database/migrations/20220619144213-create-user.js
+++ src/database/migrations/20220619144213-create-user.js
@@ -24,14 +24,6 @@ module.exports = {
       image: {
         type: Sequelize.STRING,
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
     });
   },
   down: async (queryInterface, Sequelize) => {

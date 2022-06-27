bec6e6fea80a104c065019ce5d394e6f4dce82d1
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-27 02:46:16 -0300

    feat: add create session service

---
+++ src/modules/Users/useCases/login/CreateSessionService.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const AppError = require('../../../../errors/AppError');
+
+class CreateSessionService {
+  constructor(usersRepository) {
+    this.usersRepo = usersRepository;
+    this.token = '';
+  }
+
+  async execute(loginData) {
+    const user = await this.usersRepo.getByEmailPassword(loginData);
+    if (!user) throw new AppError('Invalid fields');
+    this.generateToken(user.dataValues);
+    return this.token;
+  }
+
+  generateToken({ password, ...userWithoutPass }) {
+    console.log(userWithoutPass);
+    const jwtSecret = process.env.JWT_SECRET;
+    const jwtConfig = { expiresIn: '1d', algorithm: 'HS256' };
+    const token = jwt.sign({ data: { userWithoutPass } }, jwtSecret, jwtConfig);
+    this.token = token;
+  }
+}
+
+module.exports = CreateSessionService;

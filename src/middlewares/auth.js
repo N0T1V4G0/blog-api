0dc558eb456437f00cd279a74b488905bdf02f62
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-30 00:09:38 -0300

    feat: add auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const AppError = require('../errors/AppError');
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const authMiddleware = async (req, _res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) throw new AppError('Token not found', 401);
+
+    try {
+      const decoded = jwt.verify(token, jwtSecret);
+      req.body.userId = decoded.data.userWithoutPass.id;
+    } catch (e) {
+      throw new AppError('Expired or invalid token', 401);
+    }
+
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = authMiddleware;
